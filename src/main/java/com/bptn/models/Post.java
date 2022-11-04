87426614881fbf628d2f0e220be34cbc1147584b
berniceoa <62870943+AfiaPomaa@users.noreply.github.com>
2022-11-04 10:51:11 -0400

    Adding UserRepository interface

--- src/main/java/com/bptn/models/Post.java
+++ src/main/java/com/bptn/models/Post.java
@@ -13,34 +13,47 @@ public class Post {
 	private String postID;
 	@Column(name="\"postType\"")
 	private String postType;
+	@Column(name="\"post\"")
+	private String post;
 	@Column(name="\"usernameKey\"")
 	private String usernameKey;
 	
-	
-	public Post() {
-		super();
-	}
-	public Post(String postID, String postType, String usernameKey) {
+	public Post(String postID, String postType, String post, String usernameKey) {
 		super();
 		this.postID = postID;
 		this.postType = postType;
+		this.post = post;
 		this.usernameKey = usernameKey;
 	}
+
 	public String getPostID() {
 		return postID;
 	}
+
 	public void setPostID(String postID) {
 		this.postID = postID;
 	}
+
 	public String getPostType() {
 		return postType;
 	}
+
 	public void setPostType(String postType) {
 		this.postType = postType;
 	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
 	public String getUsernameKey() {
 		return usernameKey;
 	}
+
 	public void setUsernameKey(String usernameKey) {
 		this.usernameKey = usernameKey;
 	}
