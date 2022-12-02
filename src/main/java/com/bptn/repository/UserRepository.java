1f08b94986eea013013765969b0e8af121db0329
berniceoa <berniceowusuagyeman@icloud.com>
2022-12-02 14:20:58 -0500

    Adding PersonalDetailsController and PersonalDetailsService

--- src/main/java/com/bptn/repository/UserRepository.java
+++ src/main/java/com/bptn/repository/UserRepository.java
@@ -12,7 +12,8 @@ import com.bptn.models.UserID;
 @Repository
 public interface UserRepository extends JpaRepository<UserID, String> {
 	
-	@Query(value = "SELECT * from \"UserID\" where username=?1", nativeQuery = true)
-	List<UserID> findByUsername (String userName);
+
+	@Query(value = "select * from public.\"UserID\" where username=?1", nativeQuery = true)
+	UserID findByUsername(String userName);
 
 }
--- src/main/java/com/bptn/service/UserService.java
+++ src/main/java/com/bptn/service/UserService.java
@@ -1,35 +1,29 @@
 package com.bptn.service;
 
-import java.util.List;
-
+import com.bptn.exceptions.InvalidUserNameException;
+import com.bptn.models.UserID;
+import com.bptn.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bptn.exceptions.InvalidUserNameException;
-import com.bptn.models.UserID;
-import com.bptn.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public void validateUserId(String username) throws InvalidUserNameException{
-		
-		List<UserID> userIDlist = userRepository.findByUsername(username);
-		
-		if(userIDlist == null || userIDlist.isEmpty()) {
-			LOGGER.error("Username = {} does not exist", username);
-			throw new InvalidUserNameException("Username does not exist");
-		}
-		else {
-			LOGGER.debug("Username = {} validated", username);
+
+	public void validateUserId(String username) throws InvalidUserNameException {
+		UserID userID = userRepository.findByUsername(username);
+		if (userID == null) {
+			LOGGER.error("Username={} doesn't exist", username);
+			throw new InvalidUserNameException("Username doesn't exist");
+		} else {
+			LOGGER.debug("Username={} validated", username);
 		}
 	}
-
 }
